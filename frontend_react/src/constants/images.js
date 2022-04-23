6719c927ca4c5f171d3b20456de6d7ae9f217a16
Isen <60698913+IsenClaes@users.noreply.github.com>
2022-04-23 21:33:38 +0200

    Refactoring Assets

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,61 +1,20 @@
 import email from "../assets/email.png";
-import mobile from "../assets/mobile.png";
-import api from "../assets/api.png";
-import cpp from "../assets/cpp.png";
-import css from "../assets/css.png";
-import git from "../assets/git.png";
-import html from "../assets/html.png";
-import javascript from "../assets/javascript.png";
-import node from "../assets/node.png";
-import python from "../assets/python.png";
 import react from "../assets/react.png";
-import redux from "../assets/redux.png";
-import sass from "../assets/sass.png";
-import typescript from "../assets/typescript.png";
 import csharp from "../assets/csharp.png";
 import netcore from "../assets/netcore.png";
-import java from "../assets/java.png";
-import sql from "../assets/sql.png";
-import agile from "../assets/agile.png";
 
-import about01 from "../assets/about01.png";
-import about02 from "../assets/about02.png";
-import about03 from "../assets/about03.png";
-import about04 from "../assets/about04.png";
-
-import profile from "../assets/profile.png";
+import circle from "../assets/circle.svg";
 import profileBw from "../assets/profileBw.png";
 import profileColor from "../assets/profileColor.png";
-import circle from "../assets/circle.svg";
 import logo from "../assets/logo.png";
 
 export default {
   email,
-  mobile,
-  api,
-  cpp,
-  css,
-  git,
-  html,
-  javascript,
-  node,
-  python,
   react,
-  redux,
-  sass,
-  typescript,
-  about01,
-  about02,
-  about03,
-  about04,
-  profile,
   profileBw,
   profileColor,
-  circle,
   logo,
+  circle,
   csharp,
   netcore,
-  java,
-  sql,
-  agile,
 };
